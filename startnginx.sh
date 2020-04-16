4fa7ab3eb9b8b75d2d02033a4995f68bf121df39
John Browning <johnb@redhat.com>
2020-04-16 12:30:04 -0400

    update echo command with -e

--- startnginx.sh
+++ startnginx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 HOSTNAME=`hostname`
-echo "<footer>\n<p>Served from container: $HOSTNAME</p>\n</footer>\n</html>\n" >> index.html
+echo -e "<footer>\n<p>Served from container: $HOSTNAME</p>\n</footer>\n</html>\n" >> index.html
 
 nginx -g "daemon off;"
